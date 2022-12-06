4827527097964c07bb52867185a2de4f179e927b
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-06 18:25:40 -0500

    updated js file with changing some code

--- language.js
+++ language.js
@@ -1,20 +1,24 @@
- greet = language => 
-  (hello => hello[language] || 'Welcome')
-   ({
-    czech: 'Vitejte',
-    danish: 'Velkomst',
-    dutch: 'Welkom',
-    estonian: 'Tere tulemast',
-    finnish: 'Tervetuloa',
-    flemish: 'Welgekomen',
-    french: 'Bienvenue',
-    german: 'Willkommen',
-    irish: 'Failte',
-    italian: 'Benvenuto',
-    latvian: 'Gaidits',
-    lithuanian: 'Laukiamas',
-    polish: 'Witamy',
-    spanish: 'Bienvenido',
-    swedish: 'Valkommen',
-    welsh: 'Croeso'
-  })
+//created a variable called database and added all languages 
+var database = {
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+}
+//after i created a function that pulls the database if its true it would respond with the proper language. if not it would respond with the default language
+function greet(language) {
+return database[language] || "Welcome";
+}
